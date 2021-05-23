ba4fd9d7f192e160cbb59aaa374e8d950a2bcbab
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-05-23 14:29:12 -0300

    Inicio

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  description = 'Totvs';
+  url = 'https://www.gestaoerh.com.br/conteudo/empresas/D2e67dd0b6e22cc340e01625c9ba81241a1fd798.png';
+}
