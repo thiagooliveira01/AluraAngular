6c67efa718429678e054b5a0dfadc9efd96ee003
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-05-23 23:04:03 -0300

    API

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AluraAngular
 Repositório destinado os cursos de Angular realizados na Alura
+
+Executar o comando "npm install" para baixar as dependecias dos projetos.
+
+Para executar o projeto api utilizando o comando "npm start"
+Para executar o projeto alurapic utilizando o comando "npm start"
--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  description = 'Totvs';
-  url = 'https://www.gestaoerh.com.br/conteudo/empresas/D2e67dd0b6e22cc340e01625c9ba81241a1fd798.png';
+  photos = [
+    {
+      url: 'https://www.gestaoerh.com.br/conteudo/empresas/D2e67dd0b6e22cc340e01625c9ba81241a1fd798.png',
+      description: 'Totvs'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    }
+  ];
 }
