d1b6df5146a92facd6603a568a9034a87e17d890
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-06-23 22:57:03 -0300

    vmessage.component.html auth.service.ts

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
     declarations: [ SignInComponent ],
     imports: [
         ReactiveFormsModule,
-        CommonModule
+        CommonModule,
+        VMessageModule,
+        RouterModule
     ]
 })
 export class HomeModule { }
--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
 })
 export class SignInComponent implements OnInit {
 
-    loginForm!: FormGroup;
+    loginForm = new FormGroup({
+        userName: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required)
+      });
 
-    constructor(private formBuilder: FormBuilder) { }
-
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router) { }
+    
     ngOnInit(): void {
-        this.loginForm = this.formBuilder.group({
+    /*    this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
-        });
+        }); */
+    }
+    
+    login() {
+
+        const userName = this.loginForm.value.userName;
+        const password = this.loginForm.value.password;
+    
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                }
+            );
     }
 }
