957db848e6b4e1b4e58f85d855057107359f966c
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-06-27 22:37:55 -0300

    header.component

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
@@ -16,7 +17,8 @@ import { PhotosModule } from './photos/photos.module';
     AppRoutingModule,
     PhotosModule,
     ErrorsModule,
-    HomeModule
+    HomeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
