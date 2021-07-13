37c6b23d042b64bc46f715402edc394e63cd5c17
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-07-12 23:27:55 -0300

    Servico para inserir new.user

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower.case.validator';
+import { NewUser } from './new.user';
+import { SignUpService } from './signup.service';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
@@ -12,7 +15,9 @@ export class SignUpComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private userNotTakenValidatorService: UserNotTakenValidatorService) {}
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService : SignUpService,
+        private router : Router) {}
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
@@ -47,5 +52,15 @@ export class SignUpComponent implements OnInit {
                 ]
             ]
         });
+    }
+
+    signup() {
+        const newUser = this.signupForm.getRawValue() as NewUser;
+        this.signUpService
+            .signup(newUser)
+            .subscribe(
+                ()=> this.router.navigate(['']),
+                err => console.log(err)
+            );
     }
 }
