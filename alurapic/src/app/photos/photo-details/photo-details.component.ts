e4ef94e76e4581181070e5223d763041bfe85cdd
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-11-04 23:54:53 -0300

    Remoção de fotos e diretiva para exibir botão excluir

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Photo } from "../photo/photo";
 import { PhotoService } from "../photo/photo.service";
 import { Observable } from "rxjs";
@@ -9,18 +9,22 @@ import { PhotoComment } from "../photo/photo-comment";
     templateUrl: './photo-details.component.html'
 })
 export class PhotoDetailsComponent implements OnInit{ 
-
-    router: ActivatedRoute;
     photo$!: Observable<Photo>;
     photoId: number = 0;
 
     constructor(
         private route: ActivatedRoute,
-        private photoService: PhotoService){
-        this.router = route;
+        private photoService: PhotoService,
+        private router: Router){
     }
     ngOnInit(): void {
         this.photoId = this.route.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+    }
+
+    remove() {
+        this.photoService
+            .removePhoto(this.photoId)
+            .subscribe(() => this.router.navigate(['']));
     }
  }
