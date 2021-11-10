f76866db2c11c502a953f2393466a8d9ad382b8e
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-11-09 21:09:42 -0300

    Diferenciando Deploy

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { UserService } from '../user/user.service';
+import { environment } from 'src/environments/environment';
 
-const API_URL = 'http://localhost:3000';
+const API = environment.ApiUrl;
 
 @Injectable({
     providedIn: 'root'
@@ -16,7 +17,7 @@ export class AuthService {
 
     authenticate(userName: string, password: string) {
         return this.http
-            .post(API_URL + '/user/login', { userName, password }, {observe: 'response'} )
+            .post(API + '/user/login', { userName, password }, {observe: 'response'} )
             .pipe(tap(res =>{
                 const authToken = res.headers.get('x-access-token') || '';
                 this.userService.setToken(authToken);
