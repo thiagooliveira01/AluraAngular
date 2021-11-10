8a10afca37592fd2c31d66698875d27cd64c4209
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-11-10 17:08:08 -0300

    Upload e progresso da foto

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file!: File;
   preview!: string;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,10 +40,23 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments')?.value;
     this.photoService
     .upload(description, allowComments, this.file)
-    .subscribe(() => {
-      this.alertService.success('Upload complete', true);
+    .pipe(finalize(()=>{
       this.router.navigate(['/user', this.userService.getUserName()]);
-    });
+    }))
+    .subscribe(
+      (event: HttpEvent<any>) => {
+        if(event.type == HttpEventType.UploadProgress){
+          this.percentDone = Math.round(100 * event.loaded / event.total!);
+        }
+        else if(event.type == HttpEventType.Response){
+          this.alertService.success('Upload complete', true);
+        }      
+      },
+      err => {
+        console.log(err);
+        this.alertService.danger('Upload error!', true);
+      }
+    );
   }
 
   handleFile(event: Event) {
