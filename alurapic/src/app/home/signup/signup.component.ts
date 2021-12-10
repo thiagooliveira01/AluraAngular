730fca76c1c1c0f4852b849e333794b3a97a19a2
Thiago Oliveira <thiago.holiveira@totvs.com.br>
2021-12-09 23:33:41 -0300

    Validações e validação Cross

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -6,6 +6,7 @@ import { lowerCaseValidator } from 'src/app/shared/validators/lower.case.validat
 import { NewUser } from './new.user';
 import { SignUpService } from './signup.service';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { userNamePassword } from './username-password.validator';
 
 @Component({
     templateUrl: './signup.component.html',
@@ -55,6 +56,9 @@ export class SignUpComponent implements OnInit, AfterViewInit {
                     Validators.maxLength(14)
                 ]
             ]
+        },
+        {
+            validator: userNamePassword
         });        
     }
 
@@ -64,12 +68,14 @@ export class SignUpComponent implements OnInit, AfterViewInit {
     }
 
     signup() {
-        const newUser = this.signupForm.getRawValue() as NewUser;
-        this.signUpService
-            .signup(newUser)
-            .subscribe(
-                ()=> this.router.navigate(['']),
-                err => console.log(err)
-            );
+        if(this.signupForm.valid && !this.signupForm.pending){
+            const newUser = this.signupForm.getRawValue() as NewUser;
+            this.signUpService
+                .signup(newUser)
+                .subscribe(
+                    ()=> this.router.navigate(['']),
+                    err => console.log(err)
+                );
+        }
     }
 }
